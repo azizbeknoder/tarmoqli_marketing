b15c1286fc63a96cb7ef5ea8353b8a990d1dfddd
Lesson <lesson@intels-MacBook-Air-2.local>
2025-05-19 16:36:27 +0500

    again orders

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -2,33 +2,44 @@ import { Body, Controller, Delete, Get, Param, Post, Put, Req, UseGuards } from 
 import { OrdersService } from './orders.service';
 import { AuthGuard } from 'src/auth/auth.guard';
 import { AdminGuard } from 'src/auth/admin.guard';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller('orders')
 export class OrdersController {
     constructor(private readonly service:OrdersService){}
-    @Post('admin')
+    @Post('')
+    @ApiOperation({summary:"Yani order yaratish uchun."})
+    @ApiResponse({status:200,description:"success"})
     @UseGuards(AuthGuard)
     async createOrder(@Body() body:any, @Req() req:Request){
         const data = await this.service.createOrder(body,req)
         return data
     }
-    @Post()
+    @Get('admin')
+    @ApiOperation({summary:"Barcha orderlarni olish uchun admin huquqi mavjud"})
+    @ApiResponse({status:200,description:"success"})
     @UseGuards(AuthGuard,AdminGuard)
     async getAllOrdeForAdmin(){
         const data = await this.service.getAllOrders()
         return data
     }
     @Put(':id')
+    @ApiOperation({summary:"Order yangilash uchun"})
+    @ApiResponse({status:200,description:"success"})
     @UseGuards(AuthGuard,AdminGuard)
     update(@Param('id') id: string, @Body() body: any) {
         return this.service.updateOrder(+id, body);
     }
     @Delete(':id')
+    @ApiOperation({summary:"Order o'chirish uchun"})
+    @ApiResponse({status:200,description:"success"})
     @UseGuards(AuthGuard,AdminGuard)
     remove(@Param('id') id: string) {
         return this.service.deleteOrder(+id);
     }
     @Get('user')
+    @ApiOperation({summary:"Foydalanuvchini orderlarini ko'rish uchun /id kerak emas foydalanuvchi token orqali topiladi"})
+    @ApiResponse({status:200,description:"success"})
     @UseGuards(AuthGuard)
     async getAllOrderForUser(@Req()req:any){
         const data = await this.service.getAllOrdersForUsers(req)
