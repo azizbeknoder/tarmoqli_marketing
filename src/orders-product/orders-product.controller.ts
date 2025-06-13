5e537726a0664979a740b5da428b84243a50e6eb
Lesson <lesson@intels-MacBook-Air-2.local>
2025-06-13 18:46:33 +0500

    update orders product

--- src/orders-product/orders-product.controller.ts
+++ src/orders-product/orders-product.controller.ts
@@ -1,4 +1,43 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
+import { OrdersProductService } from './orders-product.service';
+import { ApiOperation } from '@nestjs/swagger';
+import { AuthGuard } from 'src/auth/auth.guard';
+import { AddProductOrder, CancelledOrdersProductDto, CheckedOrdersProductDto } from './dto/orders-product.dto';
+import { AdminGuard } from 'src/auth/admin.guard';
 
 @Controller('orders-product')
-export class OrdersProductController {}
+export class OrdersProductController {
+    constructor(private service:OrdersProductService){}
+    @ApiOperation({summary:"Yangi mahsulot sotib olish"})
+    @UseGuards(AuthGuard)
+    @Post()
+    async addOrderProduct(@Body() body:AddProductOrder, @Req() req:any){
+        const data = await this.service.addProductOrders(body,req)
+        return data
+    }
+    @ApiOperation({summary:'Barcha zakazlarni olish admin uchun'})
+    @UseGuards(AuthGuard,AdminGuard)
+    @Get('select-admin')
+    async getAllOrderProduct(){
+        return this.service.getAllOrdersProduct()
+    }
+    @ApiOperation({summary:"Barcha zakazlarni olsih token orqali user uchun"})
+    @UseGuards(AuthGuard)
+    @Get('select-user')
+    async getOneOrderProductByToken(@Req() req:any){
+        return this.service.getOneOrderProductByToken(req)
+    }
+    @ApiOperation({summary:"Zakazni tasdiqlash"})
+    @UseGuards(AuthGuard,AdminGuard)
+    @Post('checked')
+    async checkedOrdersProduct(@Body() body:CheckedOrdersProductDto){
+        return this.service.checkedOrdersProduct(body)
+    }
+    @ApiOperation({summary:"Zakazni rad etish"})
+    @UseGuards(AuthGuard,AdminGuard)
+    @Post('cancelled')
+    async cancelledOrdersProduct(@Body() body:CancelledOrdersProductDto){
+        return this.service.checkedOrdersProduct(body)
+    }
+
+}
--- src/orders-product/orders-product.module.ts
+++ src/orders-product/orders-product.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { OrdersProductController } from './orders-product.controller';
 import { OrdersProductService } from './orders-product.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   controllers: [OrdersProductController],
   providers: [OrdersProductService],
-  imports:[PrismaModule]
+  imports:[PrismaModule,AuthModule]
 })
 export class OrdersProductModule {}
