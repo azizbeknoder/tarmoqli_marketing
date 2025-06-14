bbb554ec5c35ab77ab2c6bf975e45cfc9131b2e9
Lesson <lesson@intels-MacBook-Air-2.local>
2025-06-14 18:14:50 +0500

    Take off update

--- src/take-off/take-off.controller.ts
+++ src/take-off/take-off.controller.ts
@@ -1,7 +1,37 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
 import { TakeOffService } from './take-off.service';
+import { ApiOperation } from '@nestjs/swagger';
+import { AuthGuard } from 'src/auth/auth.guard';
+import { AddTakeOffDto, CheckedTakeOffDto, RejectedTakeOffDto } from './dto/take-off.dto';
+import { AdminGuard } from 'src/auth/admin.guard';
 
 @Controller('take-off')
 export class TakeOffController {
     constructor(private service:TakeOffService){}
+    @ApiOperation({summary:"Pul yechib olish uchun so'rov yuborish"})
+    @UseGuards(AuthGuard)
+    @Post()
+    async addTakeOff(@Body() body:AddTakeOffDto, @Req() req:any){
+        const data = await this.service.addTakeOff(body,req)
+        return data
+    }
+    @ApiOperation({summary:"Admin barcha arizalarni ko'rishi"})
+    @UseGuards(AuthGuard,AdminGuard)
+    @Get()
+    async getAllTakeOfAdmin(){
+        return this.service.getAllTakeOff() 
+    }
+    @ApiOperation({summary:"To'lovni tasdiqlash"})
+    @UseGuards(AuthGuard,AdminGuard)
+    @Post('checked')
+    async chedkedTakeOf(@Body() body:CheckedTakeOffDto){
+        return this.service.checkedTakeOff(body)
+    }
+    @ApiOperation({summary:"To'lovni rad etish"})
+    @UseGuards(AuthGuard,AdminGuard)
+    @Post('rejected')
+    async rejectedTakeOff(@Body() body:RejectedTakeOffDto){
+        return this.service.rejectedTakeOff(body)
+    }
 }
+
--- src/take-off/take-off.module.ts
+++ src/take-off/take-off.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { TakeOffController } from './take-off.controller';
 import { TakeOffService } from './take-off.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   controllers: [TakeOffController],
   providers: [TakeOffService],
-  imports:[PrismaModule]
+  imports:[PrismaModule,AuthModule]
 })
 export class TakeOffModule {}
