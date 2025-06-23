551cb3be62e41da71596bbad6ba07050132b5616
Lesson <lesson@intels-MacBook-Air-2.local>
2025-06-23 20:47:23 +0500

    update product history

--- src/product-history/product-history.service.ts
+++ src/product-history/product-history.service.ts
@@ -6,13 +6,13 @@ import CustomError from 'src/utils/custom-error';
 export class ProductHistoryService {
     constructor(private prisma:PrismaService){}
     async getAllProductHistory(){
-        return this.prisma.ordersProduct.findMany({include:{user:true,main_products:true}})
+        return this.prisma.ordersProduct.findMany({include:{user:true,main_products:{include:{translations:true}},}})
     }
     async getProductHistoryUserToken(req:any){
         const oldUser = await this.prisma.users.findFirst({where:{email:req.user.email}})
         if(!oldUser){
             throw new CustomError(404,'User not found')
     }
-    return this.prisma.ordersProduct.findMany({where:{user_id:oldUser.id},include:{user:true,main_products:true,}})
+    return this.prisma.ordersProduct.findMany({where:{user_id:oldUser.id},include:{user:true,main_products:{include:{translations:true}},}})
     }
 }
